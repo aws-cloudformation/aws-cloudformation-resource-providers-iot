7ea56c53e9ef2d60ef47c37873febac092a093c2
Saurabh Singh <singhrdo@amazon.com>
2021-02-23 23:20:44 -0800

    Address PR comments from revison 2.

--- aws-iot-accountauditconfiguration/src/test/java/com/amazonaws/iot/accountauditconfiguration/TestConstants.java
+++ aws-iot-accountauditconfiguration/src/test/java/com/amazonaws/iot/accountauditconfiguration/TestConstants.java
@@ -5,12 +5,28 @@ import software.amazon.awssdk.services.iot.model.DescribeAccountAuditConfigurati
 import software.amazon.awssdk.services.iot.model.DescribeAccountAuditConfigurationResponse;
 import software.amazon.cloudformation.proxy.ResourceHandlerRequest;
 
+import java.util.Arrays;
+import java.util.List;
 import java.util.Map;
 
 public class TestConstants {
     static final String ACCOUNT_ID = "123456789012";
     static final String ROLE_ARN = "testRoleArn";
     static final String TARGET_ARN = "testTargetArn";
+    static final List<String> ALL_CHECKS = Arrays.asList("LOGGING_DISABLED_CHECK",
+            "CA_CERTIFICATE_EXPIRING_CHECK",
+            "CA_CERTIFICATE_KEY_QUALITY_CHECK",
+            "REVOKED_CA_CERTIFICATE_STILL_ACTIVE_CHECK",
+            "DEVICE_CERTIFICATE_EXPIRING_CHECK",
+            "DEVICE_CERTIFICATE_SHARED_CHECK",
+            "DEVICE_CERTIFICATE_KEY_QUALITY_CHECK",
+            "UNAUTHENTICATED_COGNITO_ROLE_OVERLY_PERMISSIVE_CHECK",
+            "AUTHENTICATED_COGNITO_ROLE_OVERLY_PERMISSIVE_CHECK",
+            "IOT_POLICY_OVERLY_PERMISSIVE_CHECK",
+            "IOT_ROLE_ALIAS_OVERLY_PERMISSIVE_CHECK",
+            "IOT_ROLE_ALIAS_ALLOWS_ACCESS_TO_UNUSED_SERVICES_CHECK",
+            "CONFLICTING_CLIENT_IDS_CHECK",
+            "REVOKED_DEVICE_CERTIFICATE_STILL_ACTIVE_CHECK");
 
     static final AuditCheckConfiguration ENABLED_CFN =
             AuditCheckConfiguration.builder().enabled(true).build();
@@ -85,49 +101,6 @@ public class TestConstants {
             .deviceCertificateExpiringCheck(DISABLED_CFN)
             .build();
 
-    static final ImmutableMap<String,software.amazon.awssdk.services.iot.model.AuditCheckConfiguration> ALL_CHECKS_MAP_IOT =
-            ImmutableMap.<String, software.amazon.awssdk.services.iot.model.AuditCheckConfiguration>builder()
-                    .put("LOGGING_DISABLED_CHECK", ENABLED_IOT)
-                    .put("CA_CERTIFICATE_EXPIRING_CHECK", ENABLED_IOT)
-                    .put("CA_CERTIFICATE_KEY_QUALITY_CHECK", DISABLED_IOT)
-                    .put("REVOKED_CA_CERTIFICATE_STILL_ACTIVE_CHECK", DISABLED_IOT)
-                    .put("DEVICE_CERTIFICATE_EXPIRING_CHECK", DISABLED_IOT)
-                    .put("DEVICE_CERTIFICATE_SHARED_CHECK", DISABLED_IOT)
-                    .put("DEVICE_CERTIFICATE_KEY_QUALITY_CHECK", DISABLED_IOT)
-                    .put("UNAUTHENTICATED_COGNITO_ROLE_OVERLY_PERMISSIVE_CHECK", DISABLED_IOT)
-                    .put("AUTHENTICATED_COGNITO_ROLE_OVERLY_PERMISSIVE_CHECK", DISABLED_IOT)
-                    .put("IOT_POLICY_OVERLY_PERMISSIVE_CHECK", DISABLED_IOT)
-                    .put("IOT_ROLE_ALIAS_OVERLY_PERMISSIVE_CHECK", DISABLED_IOT)
-                    .put("IOT_ROLE_ALIAS_ALLOWS_ACCESS_TO_UNUSED_SERVICES_CHECK", DISABLED_IOT)
-                    .put("CONFLICTING_CLIENT_IDS_CHECK", DISABLED_IOT)
-                    .put("REVOKED_DEVICE_CERTIFICATE_STILL_ACTIVE_CHECK", DISABLED_IOT)
-                    .build();
-
-    static final DescribeAccountAuditConfigurationResponse DESCRIBE_RESPONSE_ALL_CHECKS_PRESENT =
-            DescribeAccountAuditConfigurationResponse.builder()
-                    .auditCheckConfigurations(ALL_CHECKS_MAP_IOT)
-                    .auditNotificationTargetConfigurationsWithStrings(AUDIT_NOTIFICATION_TARGET_IOT)
-                    .roleArn(ROLE_ARN)
-                    .build();
-
-    static final AuditCheckConfigurations DESCRIBE_RESPONSE_ALL_CHECKS_PRESENT_CFN = AuditCheckConfigurations.builder()
-            .loggingDisabledCheck(ENABLED_CFN)
-            .caCertificateExpiringCheck(ENABLED_CFN)
-            .conflictingClientIdsCheck(DISABLED_CFN)
-            .caCertificateKeyQualityCheck(DISABLED_CFN)
-            .deviceCertificateExpiringCheck(DISABLED_CFN)
-            .authenticatedCognitoRoleOverlyPermissiveCheck(DISABLED_CFN)
-            .deviceCertificateKeyQualityCheck(DISABLED_CFN)
-            .deviceCertificateSharedCheck(DISABLED_CFN)
-            .iotPolicyOverlyPermissiveCheck(DISABLED_CFN)
-            .iotRoleAliasAllowsAccessToUnusedServicesCheck(DISABLED_CFN)
-            .iotRoleAliasOverlyPermissiveCheck(DISABLED_CFN)
-            .revokedCaCertificateStillActiveCheck(DISABLED_CFN)
-            .revokedDeviceCertificateStillActiveCheck(DISABLED_CFN)
-            .unauthenticatedCognitoRoleOverlyPermissiveCheck(DISABLED_CFN)
-            .build();
-
-
     static final DescribeAccountAuditConfigurationResponse DESCRIBE_RESPONSE_V1_STATE_WITH_NON_EXISTENT_KEY =
             DescribeAccountAuditConfigurationResponse.builder()
                     .auditCheckConfigurations(ImmutableMap.of(
@@ -156,6 +129,42 @@ public class TestConstants {
                 .desiredResourceState(model)
                 .logicalResourceIdentifier("doesn't matter")
                 .awsAccountId(ACCOUNT_ID)
+                .build();
+    }
+
+    static DescribeAccountAuditConfigurationResponse getDescribeAccountAuditConfigurationResponse(List<String> checksToEnable) {
+        ImmutableMap.Builder<String, software.amazon.awssdk.services.iot.model.AuditCheckConfiguration> builder = new ImmutableMap.Builder<>();
+
+        for (String check : ALL_CHECKS) {
+            if (checksToEnable.contains(check)) {
+                builder.put(check, ENABLED_IOT);
+            } else {
+                builder.put(check, DISABLED_IOT);
+            }
+        }
+        return DescribeAccountAuditConfigurationResponse.builder()
+                .auditCheckConfigurations(builder.build())
+                .auditNotificationTargetConfigurationsWithStrings(AUDIT_NOTIFICATION_TARGET_IOT)
+                .roleArn(ROLE_ARN)
+                .build();
+    }
+
+    static AuditCheckConfigurations getAuditCheckConfigurationsAllChecksDisabledCfn() {
+        return AuditCheckConfigurations.builder()
+                .loggingDisabledCheck(DISABLED_CFN)
+                .caCertificateExpiringCheck(DISABLED_CFN)
+                .conflictingClientIdsCheck(DISABLED_CFN)
+                .caCertificateKeyQualityCheck(DISABLED_CFN)
+                .deviceCertificateExpiringCheck(DISABLED_CFN)
+                .authenticatedCognitoRoleOverlyPermissiveCheck(DISABLED_CFN)
+                .deviceCertificateKeyQualityCheck(DISABLED_CFN)
+                .deviceCertificateSharedCheck(DISABLED_CFN)
+                .iotPolicyOverlyPermissiveCheck(DISABLED_CFN)
+                .iotRoleAliasAllowsAccessToUnusedServicesCheck(DISABLED_CFN)
+                .iotRoleAliasOverlyPermissiveCheck(DISABLED_CFN)
+                .revokedCaCertificateStillActiveCheck(DISABLED_CFN)
+                .revokedDeviceCertificateStillActiveCheck(DISABLED_CFN)
+                .unauthenticatedCognitoRoleOverlyPermissiveCheck(DISABLED_CFN)
                 .build();
     }
 }
