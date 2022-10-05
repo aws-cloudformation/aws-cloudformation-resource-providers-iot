ad976fdfcefebbd35693f79a740100b165236a0b
Mayank Sharma <shmayan@amazon.com>
2022-10-05 16:01:43 -0700

    remove unwanted code

--- aws-iot-thing/src/main/java/software/amazon/iot/thing/Translator.java
+++ aws-iot-thing/src/main/java/software/amazon/iot/thing/Translator.java
@@ -1,19 +1,15 @@
 package software.amazon.iot.thing;
 
-import org.apache.commons.lang3.exception.ExceptionUtils;
 import software.amazon.awssdk.http.HttpStatusCode;
 import software.amazon.awssdk.services.iot.model.AttributePayload;
 import software.amazon.awssdk.services.iot.model.ConflictingResourceUpdateException;
 import software.amazon.awssdk.services.iot.model.CreateThingRequest;
-import software.amazon.awssdk.services.iot.model.DeleteConflictException;
 import software.amazon.awssdk.services.iot.model.DeleteThingRequest;
 import software.amazon.awssdk.services.iot.model.DescribeThingRequest;
 import software.amazon.awssdk.services.iot.model.DescribeThingResponse;
-import software.amazon.awssdk.services.iot.model.InternalException;
 import software.amazon.awssdk.services.iot.model.InternalFailureException;
 import software.amazon.awssdk.services.iot.model.InvalidRequestException;
 import software.amazon.awssdk.services.iot.model.IotException;
-import software.amazon.awssdk.services.iot.model.LimitExceededException;
 import software.amazon.awssdk.services.iot.model.ListThingsRequest;
 import software.amazon.awssdk.services.iot.model.ListThingsResponse;
 import software.amazon.awssdk.services.iot.model.ResourceAlreadyExistsException;
@@ -33,10 +29,6 @@ import software.amazon.cloudformation.exceptions.CfnNotFoundException;
 import software.amazon.cloudformation.exceptions.CfnResourceConflictException;
 import software.amazon.cloudformation.exceptions.CfnServiceInternalErrorException;
 import software.amazon.cloudformation.exceptions.CfnThrottlingException;
-import software.amazon.cloudformation.proxy.HandlerErrorCode;
-import software.amazon.cloudformation.proxy.Logger;
-import software.amazon.cloudformation.proxy.OperationStatus;
-import software.amazon.cloudformation.proxy.ProgressEvent;
 
 import java.util.Collection;
 import java.util.List;
@@ -74,64 +66,6 @@ public class Translator {
             return new CfnAccessDeniedException(operationName, e);
         } else {
             return new CfnServiceInternalErrorException(operationName, e);
-        }
-    }
-
-    static ProgressEvent<ResourceModel, CallbackContext> translateExceptionToProgressEvent(
-            ResourceModel model, Exception e, Logger logger) {
-
-        HandlerErrorCode errorCode = translateExceptionToErrorCode(e, logger);
-        ProgressEvent<ResourceModel, CallbackContext> progressEvent =
-                ProgressEvent.<ResourceModel, CallbackContext>builder()
-                        .resourceModel(model)
-                        .status(OperationStatus.FAILED)
-                        .errorCode(errorCode)
-                        .build();
-        if (errorCode != HandlerErrorCode.InternalFailure) {
-            progressEvent.setMessage(e.getMessage());
-        }
-        return progressEvent;
-    }
-
-
-    static HandlerErrorCode translateExceptionToErrorCode(Exception e, Logger logger) {
-        logger.log(String.format("Translating exception \"%s\", stack trace: %s",
-                e.getMessage(), ExceptionUtils.getStackTrace(e)));
-
-        // We're handling all the exceptions documented in API docs
-        // https://docs.aws.amazon.com/iot/latest/apireference/API_CreateThing.html#API_CreateThing_Errors
-        // (+same pages for other APIs)
-        // For Throttling and InternalFailure, we want CloudFormation to retry, and it will do so based on the error code.
-        // Reference with Retryable/Terminal in comments for each: https://tinyurl.com/y378qdno
-        if (e instanceof ResourceAlreadyExistsException) {
-            return HandlerErrorCode.AlreadyExists;
-        } else if (e instanceof software.amazon.cloudformation.exceptions.ResourceAlreadyExistsException) {
-            return HandlerErrorCode.AlreadyExists;
-        } else if (e instanceof InvalidRequestException) {
-            return HandlerErrorCode.InvalidRequest;
-        } else if (e instanceof LimitExceededException) {
-            return HandlerErrorCode.ServiceLimitExceeded;
-        } else if (e instanceof UnauthorizedException) {
-            return HandlerErrorCode.AccessDenied;
-        } else if (e instanceof InternalFailureException) {
-            return HandlerErrorCode.ServiceInternalError;
-        } else if (e instanceof InternalException) {
-            return HandlerErrorCode.ServiceInternalError;
-        } else if (e instanceof ServiceUnavailableException) {
-            return HandlerErrorCode.ServiceInternalError;
-        } else if (e instanceof ThrottlingException) {
-            return HandlerErrorCode.Throttling;
-        } else if (e instanceof ResourceNotFoundException) {
-            return HandlerErrorCode.NotFound;
-        } else if (e instanceof ConflictingResourceUpdateException | e instanceof DeleteConflictException) {
-            return HandlerErrorCode.ResourceConflict;
-        } else if (e instanceof IotException && ((IotException) e).statusCode() == 403) {
-            return HandlerErrorCode.AccessDenied;
-        } else {
-            logger.log(String.format("Unexpected exception \"%s\", stack trace: %s",
-                    e.getMessage(), ExceptionUtils.getStackTrace(e)));
-            // Any other exception at this point is unexpected.
-            return HandlerErrorCode.InternalFailure;
         }
     }
 
